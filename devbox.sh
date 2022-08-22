76f581e2d62d84c933a9aec479213dcec9ae742d
Ace Eldeib <alexeldeib@gmail.com>
2022-08-22 11:33:25 -0400

    update docker

--- devbox.sh
+++ devbox.sh
@@ -120,15 +120,15 @@ curl -LO https://github.com/containerd/nerdctl/releases/download/v0.11.2/nerdctl
 tar -xvzf nerdctl-full-0.11.2-linux-amd64.tar.gz -C /usr/
 rm nerdctl-full-0.11.2-linux-amd64.tar.gz
 
-curl -LO https://download.docker.com/linux/static/stable/x86_64/docker-20.10.8.tgz
-tar -xvzf docker-20.10.8.tgz
+curl -LO https://download.docker.com/linux/static/stable/x86_64/docker-20.10.17.tgz
+tar -xvzf docker-20.10.17.tgz
 
 install -m 0555 docker/dockerd /usr/bin/dockerd
 install -m 0555 docker/docker-init /usr/bin/docker-init
 install -m 0555 docker/docker-proxy /usr/bin/docker-proxy
 install -m 0555 docker/docker /usr/bin/docker
 rm -rf docker 
-rm docker-20.10.8.tgz
+rm docker-20.10.17.tgz
 
 echo "installing rootlessctl + rootlesskit"
 WORKDIR="$(mktemp -d)"
